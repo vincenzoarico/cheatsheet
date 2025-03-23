a5628e68997f8aaaf72e7cc40e938ceefa44d13b
vincenzoarico <88898818+vincenzoarico@users.noreply.github.com>
2025-03-23 01:13:11 +0100

    Update cheatsheet.sh

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -33,7 +33,7 @@ update() {
     else
         echo "Tealdeer is already updated"
     fi
-    
+
     return 0
 }
 
@@ -41,7 +41,7 @@ update() {
 print_commands() {
     echo "My commands:"
     cat "$CHEATSHEET_COMMANDS"/*.txt 2>/dev/null
-    
+
     return 0
 }
 
@@ -52,10 +52,10 @@ print_command() {
         echo "Error: 'print_command' requires $expected_params parameter. Received $#."
         return 1
     }
-    
+
     echo "My commands:"
     cat "$CHEATSHEET_COMMANDS/$(sanitize_name "$1").txt" 2>/dev/null
-    
+
     return 0
 }
 
@@ -66,14 +66,14 @@ add_command() {
         echo "Error: 'add_command' requires $expected_params parameter. Received $#."
         return 1
     }
-    
+
     ! test -f "$CHEATSHEET_COMMANDS/$(sanitize_name "$1").txt" && touch "$CHEATSHEET_COMMANDS/$(sanitize_name "$1").txt"
     echo "$2" >> "$CHEATSHEET_COMMANDS/$(sanitize_name "$1").txt"
     echo "Description: $3" >> "$CHEATSHEET_COMMANDS/$(sanitize_name "$1").txt"
     echo "Added: $(date +%Y-%m-%d)" >> "$CHEATSHEET_COMMANDS/$(sanitize_name "$1").txt"
     echo "" >> "$CHEATSHEET_COMMANDS/$(sanitize_name "$1").txt"
     echo "Added command: '$2'"
-    
+
     return 0
 }
 
@@ -83,37 +83,41 @@ execute() {
         echo "Error: 'execute' requires $expected_params parameter. Received $#."
         return 1
     }
-    
+
     local encoded_cmd_type=$(echo "$1" | sed 's/ /%20/g')
 
     # tldr --update &>/dev/null # tldr autoupdate
 
 
     # Tealdeer
-    tealdeer_output=$(tldr "$1" 2>/dev/null)
+    tealdeer_output=$(tldr "$(sanitize_name "$1")" 2>/dev/null)
     if [[ -n "$tealdeer_output" ]]; then
-        echo "${GREEN}${SEPARATOR}\nTEALDEER\n${SEPARATOR}${RESET}"
+        echo -e "${GREEN}${SEPARATOR}TEALDEER${SEPARATOR}${RESET}"
         echo "$tealdeer_output"
-    else 
-	echo "${RED}Command not found in tldr${RESET}"
+    else
+	echo -e "${RED}Command not found in tldr${RESET}"
     fi
+
+    echo ""
 
     # cheat.sh
     cheat_output=$(curl -s "cheat.sh/$encoded_cmd_type")
     if [[ -n "$cheat_output" ]]; then
-        echo "${BLUE}${SEPARATOR}CHEAT.SH${SEPARATOR}${RESET}"
+        echo -e "${BLUE}${SEPARATOR}CHEAT.SH${SEPARATOR}${RESET}"
         echo "$cheat_output"
     else
-	echo "${RED}Failed to fetch from cheat.sh${RESET}"
+	echo -e "${RED}Failed to fetch from cheat.sh${RESET}"
     fi
+
+    echo ""
 
     # your commands
     custom_output=$(cat "$CHEATSHEET_COMMANDS/$(sanitize_name "$1").txt" 2>/dev/null)
     if [[ -n "$custom_output" ]]; then
-        echo "${YELLOW}${SEPARATOR}YOUR COMMANDS${SEPARATOR}${RESET}"
+        echo -e "${YELLOW}${SEPARATOR}YOUR COMMANDS${SEPARATOR}${RESET}"
         echo "$custom_output"
-    else 
-	echo "${RED}No personal commands found${RESET}"
+    else
+	echo -e "${RED}No personal commands found${RESET}"
     fi
 
     return 0
@@ -124,12 +128,13 @@ help(){
     echo "PRINT ALL YOUR COMMANDS: cheatsheet print_commands"
     echo "PRINT ONE TYPE OF YOUR COMMANDS: cheatsheet print_command <cmd_type>"
     echo "ADD COMMAND: cheatsheet add_command <cmd_type> <cmd> <desc>"
-    echo "CHEAT: cheatsheet execute <cmd>"
+    echo "CHEAT: cheatsheet execute <cmd_type>"
     echo "HELP: cheatsheet help"
-    
-    echo "Each command parameter has to be passed between "" if contains spaces"
-    echo "Path to manage your commands: '$CHEATSHEET_COMMANDS'"
-    
+
+    echo ""
+    echo "Each command parameter has to be passed between \"parameter\" if contains spaces"
+    echo "Path to manage your added commands: '$CHEATSHEET_COMMANDS'"
+
     return 0
 }
 
@@ -138,7 +143,7 @@ main() {
     if ! test -d "$CHEATSHEET_COMMANDS"; then
         mkdir "$CHEATSHEET_COMMANDS"
     fi
-    
+
     local function="$1"
     shift
     if declare -f "$function" &>/dev/null; then
@@ -147,7 +152,7 @@ main() {
         echo "Function '$function' doesn't exist!"
         return 1
     fi
-    
+
     return 0
 }
 
